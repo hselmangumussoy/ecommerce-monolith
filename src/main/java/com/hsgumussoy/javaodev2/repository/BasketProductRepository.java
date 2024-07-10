0b0f0427b196535a83c7ba586e0c723ded382930
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-07-10 15:05:26 +0300

    gerekli metodlar eklendi

--- src/main/java/com/hsgumussoy/javaodev2/repository/BasketProductRepository.java
+++ src/main/java/com/hsgumussoy/javaodev2/repository/BasketProductRepository.java
@@ -3,5 +3,8 @@ package com.hsgumussoy.javaodev2.repository;
 import com.hsgumussoy.javaodev2.entity.BasketProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BasketProductRepository extends JpaRepository<BasketProduct, Long> {
+    Optional<BasketProduct> findByBasketIdAndProductId(Long basketId, Long productId);
 }
--- src/main/java/com/hsgumussoy/javaodev2/service/BasketProductService.java
+++ src/main/java/com/hsgumussoy/javaodev2/service/BasketProductService.java
@@ -3,5 +3,5 @@ package com.hsgumussoy.javaodev2.service;
 import com.hsgumussoy.javaodev2.entity.BasketProduct;
 
 public interface BasketProductService {
-    void save(BasketProduct bp);
+    BasketProduct save(BasketProduct bp);
 }
