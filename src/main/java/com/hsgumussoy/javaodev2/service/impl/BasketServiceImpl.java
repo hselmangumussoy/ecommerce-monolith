e67d3c9456123e219e1de7da100429e51e905d09
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-07-13 23:38:45 +0300

    save de alreadyExistBasket ve addToBasket oluşturuldu

--- src/main/java/com/hsgumussoy/javaodev2/service/impl/BasketServiceImpl.java
+++ src/main/java/com/hsgumussoy/javaodev2/service/impl/BasketServiceImpl.java
@@ -4,25 +4,32 @@ import com.hsgumussoy.javaodev2.dto.BasketDto;
 import com.hsgumussoy.javaodev2.dto.BasketProductDto;
 import com.hsgumussoy.javaodev2.entity.Basket;
 import com.hsgumussoy.javaodev2.entity.BasketProduct;
+import com.hsgumussoy.javaodev2.entity.Product;
 import com.hsgumussoy.javaodev2.entity.User;
 import com.hsgumussoy.javaodev2.exception.RecordNotFoundExceptions;
 import com.hsgumussoy.javaodev2.mapper.BasketMapper;
 import com.hsgumussoy.javaodev2.mapper.BasketProductMapper;
+import com.hsgumussoy.javaodev2.repository.BasketProductRepository;
 import com.hsgumussoy.javaodev2.repository.BasketRepository;
 import com.hsgumussoy.javaodev2.service.BasketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class BasketServiceImpl implements BasketService {
 
     @Autowired
     private BasketRepository repository;
+    @Autowired
+    private BasketProductRepository basketProductRepository;
     @Autowired
     private BasketMapper basketMapper;
     @Autowired
@@ -30,6 +37,8 @@ public class BasketServiceImpl implements BasketService {
     private BasketProductMapper basketProductMapper;
     @Autowired
     private UserServiceImpl userService;
+    @Autowired
+    private ProductServiceImpl productService;
     @Autowired
     @Lazy
     private BasketProductServiceImpl basketProductService;
@@ -39,13 +48,24 @@ public class BasketServiceImpl implements BasketService {
     @Override
     public BasketDto save(BasketDto dto) {
         // Kullanıcıya ait açık bir sepet var mı kontrol et
-        User user = userService.findUserById(dto.getUserId());
-        Optional<Basket> existingBasket = repository.findByUserIdAndStatus(user.getId(),BASKET_STATUS_NONE);
+        User existingUser = userService.findUserById(dto.getUserId());
+        if(existingUser == null){
+            throw  new RecordNotFoundExceptions(4000, "User  not found");
+        }
 
-        Basket basket;
+        Optional<Basket> existingBasket = repository.findByUserIdAndStatus(existingUser.getId(), BASKET_STATUS_NONE);
+
+        Product existingProduct = productService.findProductById(dto.getProductId());
+
         if(existingBasket.isPresent()){
+            return basketMapper.entityToDto(alreadyExistBasket(dto, existingProduct, existingBasket.get()));
+        }else{
+            return basketMapper.entityToDto(addToBasket(dto, existingProduct,existingUser));
+        }
+        /*Basket basket;
+        if (existingBasket.isPresent()) {
             basket = existingBasket.get();
-        }else {
+        } else {
             // yeni sepet oluştur.
             basket = basketMapper.dtoToEntity(dto);
             basket.setStatus(BASKET_STATUS_NONE);
@@ -54,25 +74,81 @@ public class BasketServiceImpl implements BasketService {
             repository.save(basket);
         }
 
-        //sepete ürün ekle
-        if (dto.getBasketProductList() != null) {  // Null kontrolü
+        // Sepete ürün ekle
+        if (dto.getBasketProductList() != null && !dto.getBasketProductList().isEmpty()) {  // Null kontrolü
             for (BasketProductDto bpDto : dto.getBasketProductList()) {
-                try {
-                    BasketProduct existingBasketProduct = basketProductService.findBasketProductByBasketIdAndProductId(basket.getId(), bpDto.getProductId());
+                BasketProduct existingBasketProduct = basketProductService.findBasketProductByBasketIdAndProductId(basket.getId(), bpDto.getProductId());
+                if (existingBasketProduct != null){
+                    // Eğer ürün zaten sepette varsa, miktarı artır
                     existingBasketProduct.setCount(existingBasketProduct.getCount() + bpDto.getCount());
                     existingBasketProduct.setTotalAmount(existingBasketProduct.getTotalAmount() + bpDto.getTotalAmount());
                     basketProductService.save(existingBasketProduct);
-                } catch (IllegalArgumentException e) {
+                } else {
+                    // Yeni ürün ekleyin
                     BasketProduct bp = basketProductMapper.dtoToEntity(bpDto);
-                    bp.setBasket(basket);
+                    bp.setProduct(productService.findProductById(bpDto.getProductId()));
                     basketProductService.save(bp);
                 }
             }
         } else {
             dto.setBasketProductList(new ArrayList<>());  // Null ise boş bir liste oluştur
-        }
-        return basketMapper.entityToDto(basket);
+        }*/
 
+    }
+
+    private Basket addToBasket(BasketDto dto, Product existingProduct,User existingUser) {
+        Basket basket = new Basket();
+
+        basket.setUser(existingUser);
+        basket.setStatus(BASKET_STATUS_NONE);
+        basket.setTotalPrice(existingProduct.getPrice() * dto.getCount());
+
+        BasketProduct basketProduct = new BasketProduct();
+        basketProduct.setCount(dto.getCount());
+        basketProduct.setTotalAmount(existingProduct.getPrice() * dto.getCount());
+        basketProduct.setBasket(basket);
+        basketProduct.setProduct(existingProduct);
+
+        basketProduct = basketProductRepository.save(basketProduct);
+        basket.setBasketProductList(List.of(basketProduct));
+
+        basket =repository.save(basket);
+
+        return basket;
+    }
+
+    private Basket alreadyExistBasket(BasketDto dto, Product existingProduct, Basket basket) {
+        basket.setTotalPrice(basket.getTotalPrice() + existingProduct.getPrice()*dto.getCount());
+        List<BasketProduct> basketProductList = basket.getBasketProductList();
+
+        boolean isFindBasketProduct = false;
+        if(basketProductList != null){
+            for( BasketProduct basketProduct: basketProductList) {
+                if (Objects.equals(basketProduct.getProduct().getId(), existingProduct.getId())) {
+                    basketProduct.setCount(dto.getCount() + basketProduct.getCount());
+                    basketProduct.setTotalAmount(dto.getCount() * existingProduct.getPrice() + basketProduct.getTotalAmount());
+                    isFindBasketProduct = true;
+                    break;
+                }
+
+            }
+        }
+        if(!isFindBasketProduct){
+            BasketProduct basketProduct = new BasketProduct();
+            basketProduct.setBasket(basket);
+            basketProduct.setProduct(existingProduct);
+            basketProduct.setCount(dto.getCount());
+            basketProduct.setTotalAmount(dto.getCount() * existingProduct.getPrice());
+
+            basketProduct= basketProductRepository.save(basketProduct);
+            basketProductList.add(basketProduct);
+        }
+
+        basket.setBasketProductList(basketProductList);
+        basket = repository.save(basket);
+
+
+        return basket;
     }
 
     @Override
