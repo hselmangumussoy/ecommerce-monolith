bebb2cd8323a03a31c6e1398e758cfb7e294d61f
hselmangumussoy <153692178+hselmangumussoy@users.noreply.github.com>
2024-07-13 23:37:24 +0300

    kaydetme işlemleri eklendi

--- src/main/java/com/hsgumussoy/javaodev2/service/impl/BasketProductServiceImpl.java
+++ src/main/java/com/hsgumussoy/javaodev2/service/impl/BasketProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.hsgumussoy.javaodev2.service.impl;
 
+import com.hsgumussoy.javaodev2.entity.Basket;
 import com.hsgumussoy.javaodev2.entity.BasketProduct;
+import com.hsgumussoy.javaodev2.entity.Product;
 import com.hsgumussoy.javaodev2.repository.BasketProductRepository;
 import com.hsgumussoy.javaodev2.service.BasketProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,15 +28,20 @@ public class BasketProductServiceImpl implements BasketProductService {
         }
 
         // İlgili Basket ve Product varlıklarını doğrulayın
-        basketService.findBasketById(basketProduct.getBasket().getId());
-        productService.findProductById(basketProduct.getProduct().getId());
+        Basket basket = basketService.findBasketById(basketProduct.getBasket().getId());
+        basketProduct.getBasket().setId(basket.getId());
+
+        Product product = productService.findProductById(basketProduct.getProduct().getId());
+        basketProduct.getBasket().setId(product.getId());
 
         // Ürün miktarına göre toplam fiyatı hesaplayın
         double totalPrice = calculateTotalAmount(basketProduct.getProduct().getPrice(), basketProduct.getCount());
+        basketProduct.setTotalAmount(totalPrice);
 
         // BasketProduct varlığını kaydedin
         return repository.save(basketProduct);
     }
+
 
     public BasketProduct findBasketProductByBasketIdAndProductId(Long basketId, Long productId) {
         return repository.findByBasketIdAndProductId(basketId, productId)
